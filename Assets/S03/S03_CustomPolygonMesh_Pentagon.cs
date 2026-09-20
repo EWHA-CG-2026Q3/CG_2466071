c2f9561665f4d437d371a11cba47385e693b3730
hyewonss <shwshw2003@soongsil.ac.kr>
2026-09-20 17:22:00 +0900

    S04_위4면확인완료_c1

--- Assets/S03/S03_CustomPolygonMesh_Pentagon.cs
+++ Assets/S03/S03_CustomPolygonMesh_Pentagon.cs
@@ -21,12 +21,13 @@ public class S03_CustomPolygonMesh_Pentagon : MonoBehaviour
           0, 3, 4  
         };
 
-        Mesh mesh = new Mesh();
-        mesh.vertices = vertices;
-        mesh. triangles = triangles;
-        mesh.RecalculateNormals();
+        Mesh mesh = new Mesh();     //빈 mesh(도형) 하나 만들기
+        mesh.vertices = vertices;   //만든 정점 정보 넣기
+        mesh. triangles = triangles; //입력한 정점들로 삼각형 만들기
+        mesh.RecalculateNormals();   //면이 어느 방향을 바라보고 있는지 다시 계산(방향정보) 빛계산가능해짐
 
-        GetComponent<MeshFilter>().mesh = mesh;
-        GetComponent<MeshRenderer>().sharedMaterial = new Material(Shader.Find("Universal Render Pipeline/Lit"));
+        GetComponent<MeshFilter>().mesh = mesh; //만든 mesh 실제 게임오브젝트의 매쉬필터에 넣기(어떤 매쉬 사용할 지)
+        GetComponent<MeshRenderer>().sharedMaterial = new Material(Shader.Find("Universal Render Pipeline/Lit")); 
+        //그 mesh를 어떻게 화면에 그릴 지
     }
 }
